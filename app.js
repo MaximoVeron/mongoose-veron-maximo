713e5662a2212c8ece3ca4fff9bd2e6683367ccf
Maximo Veron <maximoveron49@gmail.com>
2025-09-13 11:18:05 -0300

    creacion de las rutas y el index para routes

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import "dotenv/config";
 import { connectDB } from './src/config/db.js';
+import { indexRouter } from './src/routes/index.js';
 
 const app = express();
 app.use(express.json());
 
 const PORT = process.env.PORT;
+
+app.use("/api", indexRouter);
 
 app.listen(PORT, async () => {
     await connectDB();
